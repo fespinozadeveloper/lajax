f12d6c9dce63cdc85279880fb3212c01c8cd549c
Thierry Feuzeu <thierry.feuzeu@gmail.com>
2016-03-20 11:24:43 +0100

    Updated the pagination.
    Xajax returns an instance of the Paginator class.

--- src/Lagdo/Lajax/Controller.php
+++ src/Lagdo/Lajax/Controller.php
@@ -39,15 +39,15 @@ class Controller
 	/**
 	 * Make the pagination for an Xajax controller method
 	 *
-	 * @param integer $currentPage the current page
-	 * @param integer $itemsPerPage the number of items per page page
 	 * @param integer $itemsTotal the total number of items
+	 * @param integer $itemsPerPage the number of items per page page
+	 * @param integer $page the current page
 	 * @param string $method the name of the method
 	 * @param array $parameters the parameters of the method
 	 * @return object the Laravel paginator instance
 	 */
-	final public function paginate($currentPage, $itemsPerPage, $itemsTotal, $method, array $parameters = array())
+	final public function paginator($itemsTotal, $itemsPerPage, $page, $method, array $parameters = array())
 	{
-		return $this->request->paginate($currentPage, $itemsPerPage, $itemsTotal, $this, $method, $parameters);
+		return $this->request->paginator($itemsTotal, $itemsPerPage, $page, $this, $method, $parameters);
 	}
 }
